22eaceb6f82f056c1f39ce845ca627b405e11cc4
Jayce Azua <jayce.azua@gmail.com>
2019-05-23 12:37:46 -0400

    Update models.py

--- recipes/models.py
+++ recipes/models.py
@@ -1,41 +1,45 @@
 from django.db import models
-from django.contrib.auth import get_user_model
-from datetime import datetime
+from django.contrib.auth import get_user_model  # gets current user
 
 # Create your models here.
-# python3 manage.py makemigrations recipes
+
 
 class Recipe(models.Model):
-    # one recipe to one user
-    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    name = models.CharField(max_length=200, null=False, blank=True, unique=True)
-    # one to many recipe
-    # one to many recipe
+  # one recipe to one user
+  owner = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, primary_key=True)
+  name = models.CharField(max_length=200, null=False, blank=True, unique=True)
+  # one to many steps
+  # one to many ingredients
+  created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.name
+  def __str__(self):
+      return self.name
 
-    class Meta:
-        verbose_name_plural = "Recipes"
+  class Meta:
+      verbose_name_plural = "Recipes"
 
 
 class Step(models.Model):
-    step_text = models.CharField(max_length=200, null=False, blank=True)
-    # many steps to one recipe
+  step_text = models.CharField(max_length=200, null=False, blank=True)
+  # many to one recipe
+  recipe = models.ForeignKey(Recipe, related_name='steps', on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.step_text
+  def __str__(self):
+      return self.step_text
 
-    class Meta:
-        verbose_name_plural = "Steps"
+  class Meta:
+      verbose_name_plural = "Steps"
+      unique_together = ('recipe', 'step_text')
 
 
 class Ingredient(models.Model):
-    text = models.CharField(max_length=200, null=False, blank=True)
-    # many ingredients to one recipe
+  text = models.CharField(max_length=200, null=False, blank=True)
+  # many to one recipe
+  recipe = models.ForeignKey(Recipe, related_name='ingredients', on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.text
+  def __str__(self):
+      return self.text
 
-    class Meta:
-        verbose_name_plural = "Ingredients"
+  class Meta:
+      verbose_name_plural = "Ingredients"
+      unique_together = ('recipe', 'text')
